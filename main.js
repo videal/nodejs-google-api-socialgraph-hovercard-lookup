93910d46e0eec7a5387ca409b776533860e2863e
a.mihaylov <a.mihaylov@videal.net>
2016-06-24 19:39:35 +0300

    1. Initial working copy.

---
+++ main.js
@@ -0,0 +1,36 @@
+var _ = require('underscore');
+module.exports = {
+    process: function (request, email, callback) {
+        var uri = 'https://apis.google.com/u/0/_/socialgraph/lookup/hovercards/?rt=j';
+        email = encodeURIComponent("[[[" + email + "]]]");
+        request.post({
+            url: uri,
+            body: "m=" + email,
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            }
+        }, function (error, response, body) {
+            var parsed = '' + body;
+            // Fix corrupt JSON coming from the service
+            parsed = parsed.replace(')]}\'\n\n', '');
+            // Parse JSON
+            parsed = JSON.parse(parsed);
+            // Extract scalar values from all nested arrays
+            parsed = _.flatten(parsed);
+            // Remove empty values
+            parsed = parsed.filter(function (item) {
+                return item != null;
+            });
+            // Look for known values
+            for (var i = 0; i < parsed.length; i++) {
+                var value = parsed[i];
+                if (/^\/\/plus.google.com\/u/.test(value)) {
+                    callback(undefined, value);
+                    return;
+                }
+            }
+            // No known data found
+            callback(undefined, null);
+        });
+    }
+};
